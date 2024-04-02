682fc75dd0e40e5a7d1a79b03fd9b3278bb120fe
gusrbstjr <gusrbstjr@naver.com>
2024-04-02 18:08:23 +0900

    Product

---
+++ src/main/java/com/PssUsChicken/shop/model/Product/Food.java
@@ -0,0 +1,14 @@
+package com.PssUsChicken.shop.model.Product;
+
+public class Food extends Product{
+
+    public Food() {
+    }
+
+    public Food(String name, String price) {
+        super(name, price);
+    }
+
+
+
+}
